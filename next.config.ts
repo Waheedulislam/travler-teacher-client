f69d8d669113a7db6735d7695248c902adbb0866
Whaeedil <baoppyhossen1234@gmail.com>
2025-06-06 11:51:27 +0600

    image

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,8 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**",
+        hostname: "traveltoyourteacher.ru",
+        pathname: "/**",
       },
     ],
   },
